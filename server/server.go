ed3751057a757b5ee97358019e0b6aa134975608
sabryassaf <sabry.assaf12@gmail.com>
2025-01-19 11:31:01 +0200

    updated makefile, added token verification via MSLib

--- server/server.go
+++ server/server.go
@@ -3,17 +3,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	gpb "github.com/BetterGR/grades-microservice/protos"
 	ms "github.com/TekClinic/MicroService-Lib"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"k8s.io/klog/v2"
 )
 
 const (
-	address            = "localhost:50051"
 	connectionProtocol = "tcp"
 )
 
@@ -27,26 +30,56 @@ type GradesServer struct {
 // GetStudentGrades returns all grades for a student.
 func (s *GradesServer) GetStudentGrades(ctx context.Context, req *gpb.StudentId) (*gpb.StudentGrades, error) {
 	logger := klog.FromContext(ctx)
+
+	_, err := s.VerifyToken(ctx, req.GetToken())
+	if err != nil {
+		return nil, fmt.Errorf("authentication failed: %w",
+			status.Error(codes.Unauthenticated, err.Error()))
+	}
+
+	if req.GetStudentId() == "123456789" {
+		grades := &gpb.StudentGrades{
+			StudentId: "123456789",
+			Courses: []*gpb.StudentCourseGrades{
+				{
+					CourseId: "236781", Exams: []*gpb.ExamGrade{
+						{Course: "236781", ExamType: "final_a", Grade: "85"},
+						{Course: "236781", ExamType: "final_b", Grade: "90"},
+					}, Homeworks: []*gpb.HomeworkGrade{
+						{Course: "236781", HomeworkNumber: "1", Grade: "100"},
+						{Course: "236781", HomeworkNumber: "2", Grade: "95"},
+						{Course: "236781", HomeworkNumber: "3", Grade: "0"},
+					},
+				},
+				{
+					CourseId: "234311", Exams: []*gpb.ExamGrade{
+						{Course: "234311", ExamType: "final_a", Grade: "100"},
+					},
+				},
+			},
+		}
+
+		logger.Info("Received request for student grades", "student_id", req.GetStudentId())
+
+		return grades, nil
+	}
+
 	grades := &gpb.StudentGrades{
-		StudentId: "123456789",
+		StudentId: "987654321",
 		Courses: []*gpb.StudentCourseGrades{
 			{
-				CourseId: "236781", Exams: []*gpb.ExamGrade{
-					{Course: "236781", ExamType: "final_a", Grade: "85"},
-					{Course: "236781", ExamType: "final_b", Grade: "90"},
+				CourseId: "236703", Exams: []*gpb.ExamGrade{
+					{Course: "236703", ExamType: "final_a", Grade: "85"},
+					{Course: "236703", ExamType: "final_b", Grade: "90"},
 				}, Homeworks: []*gpb.HomeworkGrade{
-					{Course: "236781", HomeworkNumber: "1", Grade: "100"},
-					{Course: "236781", HomeworkNumber: "2", Grade: "95"},
-					{Course: "236781", HomeworkNumber: "3", Grade: "0"},
+					{Course: "236703", HomeworkNumber: "1", Grade: "100"},
+					{Course: "236703", HomeworkNumber: "2", Grade: "95"},
+					{Course: "236703", HomeworkNumber: "3", Grade: "0"},
 				},
 			},
 			{
-				CourseId: "236990", Exams: []*gpb.ExamGrade{
-					{Course: "236990", ExamType: "final_a", Grade: "85"},
-					{Course: "236990", ExamType: "final_b", Grade: "90"},
-				}, Homeworks: []*gpb.HomeworkGrade{
-					{Course: "236990", HomeworkNumber: "1", Grade: "100"},
-					{Course: "236990", HomeworkNumber: "2", Grade: "95"},
+				CourseId: "234311", Exams: []*gpb.ExamGrade{
+					{Course: "234311", ExamType: "final_a", Grade: "99"},
 				},
 			},
 		},
@@ -62,6 +95,13 @@ func (s *GradesServer) GetCourseGrades(ctx context.Context,
 	req *gpb.GetCourseGradesRequest,
 ) (*gpb.GetCourseGradesResponse, error) {
 	logger := klog.FromContext(ctx)
+
+	_, err := s.VerifyToken(ctx, req.GetToken())
+	if err != nil {
+		return nil, fmt.Errorf("authentication failed: %w",
+			status.Error(codes.Unauthenticated, err.Error()))
+	}
+
 	grades := []*gpb.StudentCourseGrades{
 		{
 			StudentId: "1", CourseId: "23134", Exams: []*gpb.ExamGrade{
@@ -95,6 +135,13 @@ func (s *GradesServer) GetStudentCourseGrades(ctx context.Context,
 	req *gpb.GetStudentCourseGradesRequest,
 ) (*gpb.GetStudentCourseGradesResponse, error) {
 	logger := klog.FromContext(ctx)
+
+	_, err := s.VerifyToken(ctx, req.GetToken())
+	if err != nil {
+		return nil, fmt.Errorf("authentication failed: %w",
+			status.Error(codes.Unauthenticated, err.Error()))
+	}
+
 	studentCourseGrades := &gpb.StudentCourseGrades{
 		StudentId: req.GetStudentId(),
 		CourseId:  req.GetCourseId(),
@@ -117,9 +164,11 @@ func main() {
 	// init klog.
 	klog.InitFlags(nil)
 	// create a listener.
+	address := os.Getenv("GRADES_IP")
+
 	lis, err := net.Listen(connectionProtocol, address)
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		klog.Error("Failed to listen", "error", err)
 	}
 	// create a grpc server.
 	grpcServer := grpc.NewServer()
