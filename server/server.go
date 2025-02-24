4361b5c79aed448d90ce535908f06d59b06e5e52
sabryassaf <sabry.assaf12@gmail.com>
2025-02-24 16:32:36 +0200

    removed hardcoded port

--- server/server.go
+++ server/server.go
@@ -44,18 +44,18 @@ func (s *GradesServer) GetStudentGrades(ctx context.Context, req *gpb.StudentId)
 			Courses: []*gpb.StudentCourseGrades{
 				{
 					CourseId: "236781", Exams: []*gpb.ExamGrade{
-						{Course: "236781", ExamType: "final_a", Grade: "85"},
-						{Course: "236781", ExamType: "final_b", Grade: "90"},
-					}, Homeworks: []*gpb.HomeworkGrade{
-						{Course: "236781", HomeworkNumber: "1", Grade: "100"},
-						{Course: "236781", HomeworkNumber: "2", Grade: "95"},
-						{Course: "236781", HomeworkNumber: "3", Grade: "0"},
-					},
+					{Course: "236781", ExamType: "final_a", Grade: "85"},
+					{Course: "236781", ExamType: "final_b", Grade: "90"},
+				}, Homeworks: []*gpb.HomeworkGrade{
+					{Course: "236781", HomeworkNumber: "1", Grade: "100"},
+					{Course: "236781", HomeworkNumber: "2", Grade: "95"},
+					{Course: "236781", HomeworkNumber: "3", Grade: "0"},
+				},
 				},
 				{
 					CourseId: "234311", Exams: []*gpb.ExamGrade{
-						{Course: "234311", ExamType: "final_a", Grade: "100"},
-					},
+					{Course: "234311", ExamType: "final_a", Grade: "100"},
+				},
 				},
 			},
 		}
@@ -70,18 +70,18 @@ func (s *GradesServer) GetStudentGrades(ctx context.Context, req *gpb.StudentId)
 		Courses: []*gpb.StudentCourseGrades{
 			{
 				CourseId: "236703", Exams: []*gpb.ExamGrade{
-					{Course: "236703", ExamType: "final_a", Grade: "85"},
-					{Course: "236703", ExamType: "final_b", Grade: "90"},
-				}, Homeworks: []*gpb.HomeworkGrade{
-					{Course: "236703", HomeworkNumber: "1", Grade: "100"},
-					{Course: "236703", HomeworkNumber: "2", Grade: "95"},
-					{Course: "236703", HomeworkNumber: "3", Grade: "0"},
-				},
+				{Course: "236703", ExamType: "final_a", Grade: "85"},
+				{Course: "236703", ExamType: "final_b", Grade: "90"},
+			}, Homeworks: []*gpb.HomeworkGrade{
+				{Course: "236703", HomeworkNumber: "1", Grade: "100"},
+				{Course: "236703", HomeworkNumber: "2", Grade: "95"},
+				{Course: "236703", HomeworkNumber: "3", Grade: "0"},
+			},
 			},
 			{
 				CourseId: "234311", Exams: []*gpb.ExamGrade{
-					{Course: "234311", ExamType: "final_a", Grade: "99"},
-				},
+				{Course: "234311", ExamType: "final_a", Grade: "99"},
+			},
 			},
 		},
 	}
@@ -106,9 +106,9 @@ func (s *GradesServer) GetCourseGrades(ctx context.Context,
 	grades := []*gpb.StudentCourseGrades{
 		{
 			StudentId: "1", CourseId: "23134", Exams: []*gpb.ExamGrade{
-				{Course: "23134", ExamType: "final_a", Grade: "85"},
-				{Course: "23134", ExamType: "final_b", Grade: "90"},
-			},
+			{Course: "23134", ExamType: "final_a", Grade: "85"},
+			{Course: "23134", ExamType: "final_b", Grade: "90"},
+		},
 			Homeworks: []*gpb.HomeworkGrade{
 				{Course: "23134", HomeworkNumber: "1", Grade: "100"},
 				{Course: "23134", HomeworkNumber: "2", Grade: "95"},
@@ -116,9 +116,9 @@ func (s *GradesServer) GetCourseGrades(ctx context.Context,
 		},
 		{
 			StudentId: "2", CourseId: "23134", Exams: []*gpb.ExamGrade{
-				{Course: "23134", ExamType: "final_a", Grade: "90"},
-				{Course: "23134", ExamType: "final_b", Grade: "95"},
-			},
+			{Course: "23134", ExamType: "final_a", Grade: "90"},
+			{Course: "23134", ExamType: "final_b", Grade: "95"},
+		},
 			Homeworks: []*gpb.HomeworkGrade{
 				{Course: "23134", HomeworkNumber: "1", Grade: "100"},
 				{Course: "23134", HomeworkNumber: "2", Grade: "95"},
@@ -193,7 +193,7 @@ func main() {
 	// create a grpc server.
 	grpcServer := grpc.NewServer()
 	gpb.RegisterGradesServiceServer(grpcServer, server)
-	klog.Info("Grades server is running on port 50051")
+	klog.Info("Grades server is running on port " + os.Getenv("GRPC_PORT"))
 	// serve the grpc server.
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
